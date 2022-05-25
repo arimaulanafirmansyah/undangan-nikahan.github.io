ada900aac6f040cd2ba1b2baf4ffae1992522e45
Ari Maulana Firmansyah <65420719+arimaulanafirmansyah@users.noreply.github.com>
2022-05-25 17:07:51 +0700

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+jQuery(document).ready(function () {
+
+  /*
+      Background slideshow
+  */
+  $('#banner').backstretch([
+    "https://im-amf.me/nikahan/10.jpg"
+    , "https://im-amf.me/nikahan/10.jpg"
+    , "https://im-amf.me/nikahan/10.jpg"
+    , "https://im-amf.me/nikahan/10.jpg"
+  ], { duration: 3000, fade: 750 });
+});
